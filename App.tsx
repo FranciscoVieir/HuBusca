7f553aa5d0ed83f93ef5aad59049da8c070e9bc1
Francisco Vieira <franciscovieira@Francisco-Web.local>
2023-07-31 21:04:01 -0300

    fix: refactoring errors

--- App.tsx
+++ App.tsx
@@ -1,8 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import { Home } from './src/screens/Home';
+import { StatusBar } from 'react-native';
+import { ThemeProvider } from 'styled-components';
+
+import theme from './src/theme/index';
 import { Routes } from './src/routes';
 
 export default function App() {
-	return <Routes />;
+	return (
+		<ThemeProvider theme={theme}>
+			<StatusBar
+				barStyle="light-content"
+				backgroundColor="transparent"
+				translucent
+			/>
+			<Routes />
+		</ThemeProvider>
+	);
 }
